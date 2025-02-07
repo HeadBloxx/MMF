8e4b5511805da2540c98929d7e285c59b8066c23
HeadBloxx <85369300+HeadBloxx@users.noreply.github.com>
2025-02-07 21:48:30 +0100

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,13 @@
+name = input("What is your name?")
+print(name)
+
+name = "Jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+#print(len(input("What is your name")))
+usrename = input("What is your name?")
+length = len(username)
+print(length)
