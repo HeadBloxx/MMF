7fbb5e01fb7eb96e66d829994bb3243dced66b63
HeadBloxx <85369300+HeadBloxx@users.noreply.github.com>
2025-02-08 16:11:12 +0100

    Create 15.py

---
+++ 15.py
@@ -0,0 +1,15 @@
+len("Hello")
+
+print(type("Hello"))
+print(type(123))
+print(type(123.4))
+print(type(True))
+
+print(int("123")+int("456"))
+
+int()
+float()
+str()
+bool()
+
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
