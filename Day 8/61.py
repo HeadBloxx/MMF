70448c6980934cb6844325013ecced58ce8727fe
HeadBloxx <bloxx@gmail.com>
2025-03-02 12:29:28 +0100

    added 61

---
+++ Day 8/61.py
@@ -0,0 +1,12 @@
+def greet():
+    print("Hello Angela")
+    print("How do you do Jack Bauer?")
+    print("Isn't the weather nice?")
+
+greet()
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}")
+
+greet_with_name("Imran")
