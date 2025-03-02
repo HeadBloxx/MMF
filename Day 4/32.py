2c7ab801f13e21498196714e08d3afbb5a76a01c
HeadBloxx <bloxx@gmail.com>
2025-03-02 09:10:34 +0100

    a

---
+++ Day 4/32.py
@@ -0,0 +1,4 @@
+states_of_america = ["Delaware","Pennsylvania","New Jersey"]
+states_of_america[1] = "Pencilvania"
+states_of_america.extend("Angelaland","Jack Bauer Land")
+print(states_of_america)
